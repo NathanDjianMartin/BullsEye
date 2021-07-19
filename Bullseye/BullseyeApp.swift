4c8b8187504159f744d3ea3acfd7051274df5bba
DevKrazy <ndmroot-git@yahoo.fr>
2021-07-19 12:58:59 +0200

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Nathan on 19/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
